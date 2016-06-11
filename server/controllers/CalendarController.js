c2db8af4e1769e28fe12267ac18b220f4ef998f8
Will <willwyatttang@gmail.com>
2016-06-11 11:07:02 -0700

    Update to exclude ES6 syntax

--- server/controllers/CalendarController.js
+++ server/controllers/CalendarController.js
@@ -7,8 +7,12 @@ module.exports = {
     Calendar
       .where({ user: user })
       .fetchAll()
-      .then(events => res.status(200).send(events))
-      .catch(err => res.send(err));
+      .then(function(events) {
+        res.status(200).send(events)
+      })
+      .catch(function(err) { 
+        res.send(err)
+      });
   },
 
   createOrUpdate: function(req, res) {
@@ -18,19 +22,23 @@ module.exports = {
       Calendar
         .where({ id: event.id })
         .destroy()
-        .then(event => res.status(200).end())
-        .catch(err => res.send(err));
+        .then(function(event) {
+          res.status(200).end()
+        })
+        .catch(function(err) {
+          res.send(err)
+        });
       return;
     }
     var search = { start: event.start, end: event.end, title: event.title, user };
     if (event.id) search.id = event.id;
     new Calendar(search)
       .save()
-      .then(event => { console.log('here'); res.status(200).send(event); })
-      .catch(err => { console.log(err); res.send(err); });
-  },
-
-  remove: function(event) {
-    return 
+      .then(function(event) {
+        res.status(200).send(event); 
+      })
+      .catch(function(err) { 
+        res.send(err); 
+      });
   }
 };
